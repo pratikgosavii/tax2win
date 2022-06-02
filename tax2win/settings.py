2b7729eca6ba97021ac012e5b6f983a94a8e6ef7
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2022-06-03 02:06:58 +0530

    update

--- tax2win/settings.py
+++ tax2win/settings.py
@@ -122,9 +122,9 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # AUTH_USER_MODEL = 'users.User'
