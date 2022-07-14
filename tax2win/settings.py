cfcc6cf9191a2d59386f619f726ec9a9e41e0e05
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2022-07-14 19:03:23 +0530

    updatE

--- tax2win/settings.py
+++ tax2win/settings.py
@@ -124,7 +124,6 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
@@ -140,6 +139,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = '/root/tax2win/media/'
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
