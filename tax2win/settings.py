9b44b22ef985c769e4ffe91f2d33c7935147132a
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2022-07-16 13:54:48 +0530

    updatE

--- tax2win/settings.py
+++ tax2win/settings.py
@@ -144,7 +144,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+
